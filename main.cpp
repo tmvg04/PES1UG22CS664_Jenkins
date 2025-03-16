f22e233777f99de7678f983832b665d1d0e9bc5f
tmvg04 <143010570+tmvg04@users.noreply.github.com>
2025-03-16 22:54:09 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-  cout<<"PES1UG22CS664\n";
-  return 0;
+
+int main() {
+    cout << "PES1UG22CS664\n";
+    return 0;
 }
